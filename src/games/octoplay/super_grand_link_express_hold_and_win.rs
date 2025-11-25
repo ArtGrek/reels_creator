5ccebcbf86061209daf4f72cc3b6f8289bbe81f2
John Smith <j000s0000@mail.ru>
2025-11-25 10:34:21 +0300

    hold and win reels

--- src/games/octoplay/super_grand_link_express_hold_and_win.rs
+++ src/games/octoplay/super_grand_link_express_hold_and_win.rs
@@ -5,46 +5,43 @@ use std::collections::HashMap;
 
 pub fn extract_spin_combos() {
     let transactions: Vec<Value> = load_transactions("../data/hold_and_win/transactions/".to_string());
-
-    let mut columns: Vec<HashMap<Vec<i64>, u64>> = Vec::new();
+    let mut combos: Vec<HashMap<Vec<i64>, u64>> = Vec::new();
     for tr in &transactions {
         if let Some(reels) = tr["out"]["result"]["game"]["spins"][0]["spinData"]["reels"].as_array() {
             for (col_idx, col_val) in reels.iter().enumerate() {
                 if let Some(col_arr) = col_val.as_array() {
-                    let mut col_vec: Vec<i64> = Vec::with_capacity(col_arr.len());
+                    let mut col_vec: Vec<i64> = Vec::with_capacity(3);
                     let mut valid = true;
-                    for v in col_arr {
+                    for v in &col_arr[3..=5] {
                         if let Some(n) = v.as_i64() {
                             if n >= 10 { valid = false; break; }
                             col_vec.push(n);
-                        } else {
-                            valid = false;
-                            break;
-                        }
+                        } else {valid = false; break;}
                     }
                     if !valid { continue; }
-                    while columns.len() <= col_idx {
-                        columns.push(HashMap::new());
-                    }
-                    let map = &mut columns[col_idx];
+                    while combos.len() <= col_idx {combos.push(HashMap::new());}
+                    let map = &mut combos[col_idx];
                     *map.entry(col_vec).or_insert(0) += 1;
                 }
             }
         }
     }
-
-    let reels_parts = columns.iter().enumerate().map(|(i, col)| {
-        let mut combos: Vec<(&Vec<i64>, &u64)> = col.iter().collect();
-        //combos.sort_by(|(a, _), (b, _)| a.cmp(b));
-        combos.sort_by(|(_, c1), (_, c2)| c1.cmp(c2));
+    let spin_combos = combos.iter_mut().map(|col| {
+        let total_col_count: u64 = col.values().sum();
+        let mut col_count = 0;
+        let mut combos_col: Vec<(&Vec<i64>, &u64)> = col.iter().collect();
+        combos_col.sort_by(|(_, c1), (_, c2)| c1.cmp(c2));
         format!("\t\t\t{{\n{}\n\t\t\t}}", 
-            combos.iter().map(|(combo, count)| {
-                format!("\t\t\t\t\"{}\": [{}]", count, combo.iter().map(|n| {n.to_string()}).collect::<Vec<_>>().join(","))
+            combos_col.iter().map(|(combo, count)| {
+                col_count += *count;
+                format!("\t\t\t\t\"{}\": [{}]", 
+                    (col_count as f64 * 100.0 / total_col_count as f64 * 10000.0) as i64, 
+                    combo.iter().map(|n| {n.to_string()}).collect::<Vec<_>>().join(",")
+                )
             }).collect::<Vec<String>>().join(",\n")
         )
     }).collect::<Vec<String>>().join(",\n");
-
-    save_content("../data/hold_and_win/reels/spin_combos.json".to_string(), format!("{{\n\t\"reels\":[\n\t\t[\n{}\n\t\t]\n\t]\n}}", reels_parts),);
+    save_content("../data/hold_and_win/reels/spin_combos.json".to_string(), format!("{{\n\t\"reels\":[\n\t\t[\n{}\n\t\t]\n\t]\n}}", spin_combos),);
 }
 
 
